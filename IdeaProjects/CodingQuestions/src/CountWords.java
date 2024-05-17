8883c2dd481ffaf123b79b2e1e594d756405810e
Ujjwal <ujjcgl16@gmail.com>
2024-05-17 15:26:29 +0530

    Coding Question

---
+++ IdeaProjects/CodingQuestions/src/CountWords.java
@@ -0,0 +1,8 @@
+public class CountWords {
+    public static void main(String[] args) {
+        String s1 = "Hello, I am ujjwal Thakur";
+
+        int count = s1.length();
+        System.out.println(count);
+    }
+}
